962b8393e947c52e054b7f9a07a465da2babe1cf
Hasset Admasu <188715591+Hasset-hub@users.noreply.github.com>
2025-10-18 15:38:07 +0000

    Fix imports

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,18 +1,8 @@
-import pytest
-from presidio_anonymizer.sample import sample_run_anonymizer
+from presidio_anonymizer import sample
 
 def test_sample_run_anonymizer():
-    
     result = sample.sample_run_anonymizer("My name is Bond.", 11, 15)
     assert result.text == "My name is BIP."
     assert len(result.items) == 1
     assert result.items[0].start == 11
     assert result.items[0].end == 14
-
-
-
-
-
-
-
-
